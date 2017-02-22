640d4cb2adcced0cc65c1913222162b4615eb87d
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-02-21 21:57:37 -0800

    got something on the page

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
-const test = require('./module1.js');
+import { test, element1 } from './module1';
+// const test = require('./module1.js');
 
-console.warn('Test: ', test);
+console.warn('Test: ', element1);
 
 const app = document.getElementById('app');
+
+app.append(element1);
--- public/module1.js
+++ public/module1.js
@@ -1,1 +1,13 @@
 export const test = 'test 123';
+
+const createH1 = (text, id, className) => {
+  const h1 = document.createElement('h1');
+
+  h1.id = id || '';
+  h1.classList.add(className || '');
+  h1.innerHTML = text;
+
+  return h1;
+};
+
+export const element1 = createH1(test, 'test1', 'intros');
